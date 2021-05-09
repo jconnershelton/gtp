e66be6a0614cc940118e7b8887ac57ed7e2f353e
jconnershelton <j.conner.shelton@gmail.com>
2021-05-08 22:45:58 -0400

    Rename parameters in serve header

--- include/GTP/serve.h
+++ include/GTP/serve.h
@@ -3,4 +3,4 @@
 #include "GTP/buffer.h"
 #include <stdbool.h>
 
-void GTP_Serve(unsigned short port, GTPBuffer *(*processBuffer)(const GTPBuffer *buffer, bool *serving));
+void GTP_Serve(unsigned short port, GTPBuffer *(*processRequest)(const GTPBuffer *request, bool *serving));
