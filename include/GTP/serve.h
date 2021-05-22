b32d1a694ae5eaf3119557c6c94005f7d5f6b4ab
jconnershelton <j.conner.shelton@gmail.com>
2021-05-22 12:00:41 -0400

    Add GTP_Close Put server loop in separate thread

--- include/GTP/serve.h
+++ include/GTP/serve.h
@@ -8,7 +8,8 @@ extern "C" {
 #include "GTP/buffer.h"
 #include <stdbool.h>
 
-void GTP_Serve(unsigned short port, GTPBuffer *(*processRequest)(const GTPBuffer *request, bool *serving));
+void GTP_Serve(unsigned short port, GTPBuffer *(*processRequest)(const GTPBuffer *request));
+void GTP_Close(void);
 
 // End of the extern C block
 #ifdef __cplusplus
--- src/serve.c
+++ src/serve.c
@@ -1,31 +1,30 @@
 #include "GTP/serve.h"
 #include "readwrite.h"
 
+#include <pthread.h>
+
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-void GTP_Serve(unsigned short port, GTPBuffer *(*processBuffer)(const GTPBuffer *buffer, bool *serving)) {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) return;
-    
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(port);
+#include <stdio.h> // TODO: REMOVE
 
+static bool GTP_running = false;
+
+static pthread_t GTP_server_thread;
+static struct sockaddr_in addr;
+static int sockfd;
+
+static void *serveLoop(void *args) {
+    GTPBuffer *(*processBuffer)(const GTPBuffer *buffer) = args;
     socklen_t addrlen = sizeof(addr);
 
-    if (bind(sockfd, (struct sockaddr*) &addr, addrlen) != 0) return;
-    if (listen(sockfd, 10) != 0) return; // Edit to increase max queue
-
-    bool serving = true;
-    while (serving) {
+    while (GTP_running) {
         int connfd = accept(sockfd, (struct sockaddr*) &addr, &addrlen);
         if (connfd == -1) continue;
 
         GTPBuffer *inBuffer = readGTPBuffer(connfd);
-        GTPBuffer *outBuffer = processBuffer(inBuffer, &serving);
+        GTPBuffer *outBuffer = processBuffer(inBuffer);
         writeGTPBuffer(outBuffer, connfd);
 
         GTPBuffer_Free(inBuffer);
@@ -34,6 +33,27 @@ void GTP_Serve(unsigned short port, GTPBuffer *(*processBuffer)(const GTPBuffer 
         close(connfd);
     }
 
-    close(sockfd);
+    return NULL;
 }
 
+void GTP_Serve(unsigned short port, GTPBuffer *(*processBuffer)(const GTPBuffer *buffer)) {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) return;
+    
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    socklen_t addrlen = sizeof(addr);
+
+    if (bind(sockfd, (struct sockaddr *) &addr, addrlen) != 0) return;
+    if (listen(sockfd, 10) != 0) return; // Edit to increase max queue
+    
+    pthread_create(&GTP_server_thread, NULL, serveLoop, &addr);
+}
+
+void GTP_Close(void) {
+    GTP_running = false;
+    close(sockfd);
+    pthread_join(GTP_server_thread, NULL);
+}
